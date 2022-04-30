21ca8e2be094ae6ab3ffbcdca1746a8a01c4da63
Viswanath <alapativiswanath1@gmail.com>
2022-04-30 12:27:16 +0530

    updates

--- backend/members/api/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("login_user/", views.login_user.as_view()),
-    path("logout_user/", views.logout_user.as_view()),
-    path("register_user/", views.register_user.as_view()),
-]
--- backend/store/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-
-urlpatterns = [
-    path('posts/<str:year>/<str:month>/<str:day>/<str:name>', views.getPostImage),
-    path('products/<str:year>/<str:month>/<str:day>/<str:name>', views.getProductImage),
-]
